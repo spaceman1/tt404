cc38d1bd8ad18393d85a4bafe694f21b3bd8020e
Ryan McNally <embeddedcoder@mac.com>
2011-08-21 16:11:42 -0400

    Added settings to control pref access

--- conf.py
+++ conf.py
@@ -1,12 +1,36 @@
 # The list of library sections to hide
-libBlacklist = ['9', 'some other id']
+libBlacklist = ['some id', 'some other id']
+
 # The list of plug-ins to hide
 pluginBlacklist = ['some id', 'some other id']
+
 # Completely block the web manager
 noManage = False
+
 # Completely block filesystem browsing through /services/browse
 noBrowse = False
+
 # Block all plug-ins that aren't listed on plex online. You can still block additional plug-ins with pluginBlacklist
 plexOnlineOnly = False
+
 # Set the age of the viewer when filtering libraries. Set to 0 to disable.
-viewerAge = 15
+viewerAge = 0
+
+# Plug-in Preferences settings
+# The values correspond to: read write
+# Read: Users can see the value you've set the preference to
+# Write: Users can set a new value
+
+prefsAccess = {
+	'read': True,
+	'write': True
+}
+
+securePrefsAccess = {
+	'read': True,
+	'write': False
+}
+
+# Make the preferences completely stealth. (No info on which plug-ins have prefs, no way to list their prefs, @future: removal of prefs items from MediaContainers)
+# True will override all other prefs settings
+noPrefs = False
